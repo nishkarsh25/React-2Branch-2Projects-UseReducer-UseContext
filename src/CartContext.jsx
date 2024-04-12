361e00ed84dc34cf49221c9453f9eab25fdbba25
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-13 04:01:14 +0530

    Reducer Function:
    The cartReducer function is a typical reducer function used with React's useReducer hook. It takes the current state and an action, then returns a new state based on the action type.
    Inside the reducer, a switch statement is used to determine how to update the state based on the type of action received.
    For the 'ADD_TO_CART' action, it adds the item specified in the action payload to the cartItems array.
    For the 'UPDATE_QUANTITY' action, it updates the quantity of the specified item in the cartItems array.
    For the 'REMOVE_FROM_CART' action, it removes the specified item from the cartItems array.

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -10,7 +10,27 @@ export function CartProvider({children}){
     
 
     function cartReducer(state,action){
-        
+        console.log(state,action)
+        switch(action.type){
+            case 'ADD_TO_CART':
+                return{
+                    ...state,
+                    cartItems:[...state.cartItems,action.payload]
+                };
+            case 'UPDATE_QUANTITY':
+                return{
+                    ...state,
+                    cartItems:state.cartItems.map((item)=>item.id=== action.payload.id?{...item,quantity:action.payload.quantity}:item)
+                };
+    
+            case 'REMOVE_FROM_CART':
+                return{
+                    ...state,
+                    cartItems:state.cartItems.filter((item)=> item.id !== action.payload),
+                };
+            default:
+                return state;
+        }
     }
 
     const [cartState,cartDispatch] = useReducer(cartReducer,initialState);
