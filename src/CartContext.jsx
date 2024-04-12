b54c8bfe14e39152408df46707dc3a9869274a3e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-13 04:02:19 +0530

    Custom Hook:
    The useCart custom hook is defined to conveniently access the cart state and dispatch function within components. It uses the useContext hook to retrieve the context value provided by CartContext.Provider.

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -9,33 +9,35 @@ export function CartProvider({children}){
     }
     
 
-    function cartReducer(state,action){
-        console.log(state,action)
-        switch(action.type){
-            case 'ADD_TO_CART':
-                return{
-                    ...state,
-                    cartItems:[...state.cartItems,action.payload]
-                };
-            case 'UPDATE_QUANTITY':
-                return{
-                    ...state,
-                    cartItems:state.cartItems.map((item)=>item.id=== action.payload.id?{...item,quantity:action.payload.quantity}:item)
-                };
-    
-            case 'REMOVE_FROM_CART':
-                return{
-                    ...state,
-                    cartItems:state.cartItems.filter((item)=> item.id !== action.payload),
-                };
-            default:
-                return state;
-        }
-    }
+ function cartReducer(state,action){
+    console.log(state,action)
+    switch(action.type){
+        case 'ADD_TO_CART':
+            return{
+                ...state,
+                cartItems:[...state.cartItems,action.payload]
+            };
+        case 'UPDATE_QUANTITY':
+            return{
+                ...state,
+                cartItems:state.cartItems.map((item)=>item.id=== action.payload.id?{...item,quantity:action.payload.quantity}:item)
+            };
 
-    
+        case 'REMOVE_FROM_CART':
+            return{
+                ...state,
+                cartItems:state.cartItems.filter((item)=> item.id !== action.payload),
+            };
+        default:
+            return state;
+    }
+ }
+
     const [cartState,cartDispatch] = useReducer(cartReducer,initialState);
 
     return <CartContext.Provider value={{cartState,cartDispatch}}>{children}</CartContext.Provider>
 }
 
+export function useCart(){
+    return useContext(CartContext);
+}
