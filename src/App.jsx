e83ca52ebfb016cec73e1a83eb88e0e67978fa74
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-13 04:29:13 +0530

    Reducer Function:
    paginationReducer: This function defines how the pagination state should be updated based on different actions. It handles actions like setting the current page and updating the total number of items.

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,14 @@ import React, { useEffect, useReducer } from 'react';
 
 const itemsPerPage = 5;
 
+const paginationReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_PAGE':
+      return { ...state, currentPage: action.payload };
+    case 'SET_TOTAL_ITEMS':
+      return { ...state, totalItems: action.payload };
+    default:
+      return state;
+  }
+};
+
