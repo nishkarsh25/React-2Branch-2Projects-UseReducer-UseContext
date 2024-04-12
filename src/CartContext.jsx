de5d0461d3c730349d5b0575c7b2bdd32065ef5f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-04-13 04:00:28 +0530

    Provider Component:
    The CartProvider component is defined as a functional component that takes a children prop. This component will wrap its children with the CartContext.Provider component, making the cart state available to all descendent components.
    Inside CartProvider, an initial state for the shopping cart is defined. The state includes a cartItems array, initially empty, to store the items in the shopping cart.
    A reducer function (cartReducer) is defined to manage state updates based on dispatched actions. The reducer handles actions like adding items to the cart, updating quantities, and removing items.

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -1,3 +1,20 @@
 import { createContext, useContext, useReducer } from "react";
 
 const CartContext = createContext();
+
+export function CartProvider({children}){
+
+    const initialState = {
+        cartItems:[]
+    }
+    
+
+    function cartReducer(state,action){
+        
+    }
+
+    const [cartState,cartDispatch] = useReducer(cartReducer,initialState);
+
+    return <CartContext.Provider value={{cartState,cartDispatch}}>{children}</CartContext.Provider>
+}
+
